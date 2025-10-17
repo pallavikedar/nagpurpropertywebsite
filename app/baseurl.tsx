9bae92638cf89542de533395f57901f77d2ffacf
Pallavi <kedarpallavi723@gmail.com>
2025-10-17 17:23:56 +0530

    chnages

--- app/baseurl.tsx
+++ app/baseurl.tsx
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://82.29.161.171:8090/api";
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL ||"https://apiwebsite.sgroup.space";
